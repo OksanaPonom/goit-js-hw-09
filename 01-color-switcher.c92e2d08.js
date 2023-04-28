96b167abe445d5886ab55a8c443ccff940d0c5b5
OksanaPonom <121178306+OksanaPonom@users.noreply.github.com>
2023-04-28 16:20:15 +0000

    Deploying to gh-pages from @ OksanaPonom/goit-js-hw-09@28713d4af88c7ccdaff84daaf4ccb00eadbda2bb 🚀

---
+++ 01-color-switcher.c92e2d08.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body,o=0;t.addEventListener("click",(function(){if(0!==o)return;o=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),o=0}))}();
+//# sourceMappingURL=01-color-switcher.c92e2d08.js.map
